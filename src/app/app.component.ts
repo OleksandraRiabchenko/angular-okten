ec7aa4496aeb9b4491d023335a52a63773194a5d
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-07-26 18:36:50 +0300

    lesson1 part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {UserService} from "./services/user.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-okten';
+
+  // Розгорнута структура запису!!!
+
+// private userService: UserService;
+//
+//   constructor(userService:UserService) {
+//     this.userService = userService;
+//     this.userService.doSomeStuff();
+//   }
+
+  // Коротка форма запису
+  // constructor(private userService: UserService) {
+  //   this.userService.doSomeStuff()
+  // }
+
 }
