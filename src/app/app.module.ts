52b3b8b3e7fdeb97181505e570ecdd30ce12914e
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-07-28 18:30:48 +0300

    practice2_user_with_posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {routes} from "./routes/basic.routes";
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserWithPostsComponent } from './components/user-with-posts/user-with-posts.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserWithPostsComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
